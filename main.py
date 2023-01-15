7a2482d74783f9bd3418756fcc2e04ff4730b98f
simsimbutdifferent <simeon.campbell@live.com>
2023-01-15 14:30:07 +0700

    Surface rates json

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import requests
 import json
-
+import time
 import func_triangular_arb
 
 """ Retrive Graph QL mid prices for uniswap """
@@ -29,6 +29,22 @@ def retrieve_uniswap_information():
     return json_dict
 
 if __name__ == "__main__":
+    # to automate put all main function within a while loop, with a time.sleep(10) at the bottom.
+
     pairs = retrieve_uniswap_information()["data"]["pools"]
-    structured_pairs = func_triangular_arb.structure_trading_pairs(pairs)
-    #print(pairs[0:3])
+    structured_pairs = func_triangular_arb.structure_trading_pairs(pairs, limit = 100)
+
+    # Get surface rates
+    surface_rates_list = []
+    for t_pair in structured_pairs:
+        surface_rate = func_triangular_arb.calc_triangular_arb_surface_rate(t_pair, min_rate=0)
+        if len(surface_rate) > 0:
+            surface_rates_list.append(surface_rate)
+
+    # Save to json file
+    if len(surface_rates_list) > 0:
+        with open("uniswap_surface_rates.json", "w") as fp:
+            json.dump(surface_rates_list, fp)
+            print("File saved!")
+
+    # time.sleep(10)
